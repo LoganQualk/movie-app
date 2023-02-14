dab7a633863a9976963aa06fbdab212b86158fe2
Logan <loganq@gmail.com>
2023-02-13 22:27:16 -0500

    added input and getting value from it

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [message, setMessage] = useState('');
+  const [updated, setUpdated] = useState(message);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  }
+
+  const handleClick = () => {
+    setUpdated(message);
+    console.log(message);
+  }
 
   return (
-    <div>HEY SIMON</div>
+    <div>
+      <h1>Hey Simon</h1>
+      <input type="text" id="searchInput" onChange={handleChange} defaultValue={message}/>
+      <button onClick={handleClick}>Search</button>
+    </div>
+    
   )
 }
 
