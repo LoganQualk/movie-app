fee26a9c01ef0d4a3ee5d514c4c329dbaf324359
Logan <loganq@gmail.com>
2023-03-08 22:50:28 -0500

    used useEffect to reset variables at end of game

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { getMovie } from './components/api'
 import './App.css'
 
 function App() {
   const title = useRef(null); //this isn't the starting value of the input - it is the starting value of the reference - use at a specific moment -- useState is when things are constantly changing
   const [updated, setUpdated] = useState(null);
+  const [score, setScore] = useState(0);
+  const [guesses, setGuesses] = useState(0);
+  const [win, setWin] = useState(false);
+
+  useEffect(() => {
+    if (guesses >= 3) {
+      console.log(`Your score was: ${score} after ${guesses} guesses`);
+      setGuesses(0);
+      setScore(0);
+      setWin(false);
+    }
+  }, [guesses])
+
+  // const searchInput = document.getElementById("searchInput");
+  // searchInput.addEventListener("keydown", function(e) {
+  //   if (e.key === "Enter") {
+  //     handleClick;
+  //   }
+  // })
 
   const handleClick = async () => {
     let result = await getMovie(title.current.value);
     setUpdated(result);
-    console.log(result);
+    // console.log(score);
+    setScore(score + parseInt(result.Metascore));
+    // console.log(result.Metascore);
+    // console.log(score);
+    setGuesses(guesses+1);
+    if (score > 224 && score < 251) {
+        setWin(true);
+        console.log(win);
+    } else {
+        console.log(win);
+    }
   }
 
   return (
@@ -17,6 +46,12 @@ function App() {
       <h1>Hey Simon</h1>
       <input ref={title} type="text" id="searchInput"/>
       <button onClick={handleClick}>Search</button>
+      <br />
+      <div>SCORE: {score}</div>
+      <br />
+      <div>GUESSES: {guesses}</div>
+      <br />
+      <div>WIN: {win}</div>
     </div>
     
   )
