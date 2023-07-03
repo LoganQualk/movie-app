781127905d7edfccd2835e9539ac8a735af3d7ee
Logan <loganq@gmail.com>
2023-07-03 00:29:13 -0400

    Implemented restart game

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,79 @@ import './App.css'
 
 function App() {
   const title = useRef(null); //this isn't the starting value of the input - it is the starting value of the reference - use at a specific moment -- useState is when things are constantly changing
-  const [updated, setUpdated] = useState(null);
   const [score, setScore] = useState(0);
   const [guesses, setGuesses] = useState(0);
   const [win, setWin] = useState(false);
+  const [images, setImage] = useState([]);
 
   useEffect(() => {
     if (guesses >= 3) {
-      console.log(`Your score was: ${score} after ${guesses} guesses`);
-      setGuesses(0);
-      setScore(0);
-      setWin(false);
-    }
-  }, [guesses])
-
-  // const searchInput = document.getElementById("searchInput");
-  // searchInput.addEventListener("keydown", function(e) {
-  //   if (e.key === "Enter") {
-  //     handleClick;
-  //   }
-  // })
-
-  const handleClick = async () => {
-    let result = await getMovie(title.current.value);
-    setUpdated(result);
-    // console.log(score);
-    setScore(score + parseInt(result.Metascore));
-    // console.log(result.Metascore);
-    // console.log(score);
-    setGuesses(guesses+1);
-    if (score > 224 && score < 251) {
+      if (score > 224 && score < 251) {
         setWin(true);
-        console.log(win);
-    } else {
-        console.log(win);
+      } else {
+        const imagesContainer = document.getElementById("imagesContainer");
+        console.log(imagesContainer);
+        // imagesContainer.remove();
+        setGuesses(0);
+        setScore(0);
+        setImage([]);
+        alert("You Lost! Try again.");
+        // console.log(images);
+      }
+      // console.log(win);
+      // setGuesses(0);
+      // setScore(0);
+      // setWin(false); // probably want to move all of this to a button like "start new game"
     }
+    // console.log(guesses);
+    // console.log(images);
+  }, [guesses])
+  
+  // useEffect(() => {
+  //   console.log(win);
+  // }, [win])
+
+  const handleSearch = async () => {
+    let result = await getMovie(title.current.value);
+    setScore(score + parseInt(result.Metascore));
+    setGuesses(guesses + 1); //setScore and setGuesses should be separated --- could make another useEffect that looks at score that calls setGuesses
+    setImage(images => [...images, `${result.Poster}`]);
+    // console.log(images);
+    // console.log(guesses);
+  }
+
+  const handleRestart = async () => {
+    const imagesContainer = document.getElementById("imagesContainer");
+    console.log(imagesContainer);
+    // imagesContainer.remove();
+    setGuesses(0);
+    setImage([]);
+    setWin(false);
+    setScore(0);
   }
 
   return (
-    <div>
-      <h1>Hey Simon</h1>
-      <input ref={title} type="text" id="searchInput"/>
-      <button onClick={handleClick}>Search</button>
-      <br />
-      <div>SCORE: {score}</div>
-      <br />
-      <div>GUESSES: {guesses}</div>
-      <br />
-      <div>WIN: {win}</div>
+    <div className='mainContainer flex justify-center'>
+      <div className='flex justify-center searchContainer text-center'>
+        <h1 id='mainHeader' className='whiteText'>Welcome to my movie app. Search for a movie, the goal is to get between 225 and 250 Metacritic score. You only get 3 guesses!</h1>
+        <input ref={title} type="text" id="searchInput" />
+        <button id="searchButton" onClick={handleSearch}>Search</button>
+        <br />
+        <div id='scoreHeader' className='whiteText'>SCORE: {score}</div>
+        <br />
+        <div id='guessesHeader' className='whiteText'>GUESSES: {guesses}</div>
+        <br />
+        <div id='winHeader' className='whiteText'>WIN: {win.toString().toLocaleUpperCase()}</div>
+        {win && <div className='whiteText'>YOU WIN!!!</div>}
+        <button id="restartButton" onClick={handleRestart}>Restart Game</button>
+      </div>
+      <div id="imagesContainer">
+        {images.map((image, index) => (
+          <img key={index} src={image} alt="" /> //if you use curly braces then you have to use return because it will return with an object in it -- if you use parens then there is no object and you are just returning it by itself
+        ))} 
+      </div>
     </div>
-    
+
   )
 }
 
